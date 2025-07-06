e2332fa37ddc1a080cacc6027da0d4010afabf7a
Jinacker <rlawls1448@naver.com>
2025-07-06 16:01:49 +0900

    [FIX] gitignore 파일 적용

--- familog/Family/admin.py
+++
@@ -1,9 +0,0 @@
-# Family/admin.py
-from django.contrib import admin
-from .models import Family
-
-
-@admin.register(Family)
-class FamilyAdmin(admin.ModelAdmin):
-    list_display  = ("code", "name", "seeds")
-    search_fields = ("code", "name")
