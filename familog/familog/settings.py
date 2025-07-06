fe566f66c5b0d869c587f4901f589577ff33007f
Jinacker <rlawls1448@naver.com>
2025-07-06 20:46:43 +0900

    [FIX] 임시배포용 호스트 허용

--- familog/familog/settings.py
+++ familog/familog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-00=xn4jt0d895c(*+_s-(nhlwo4%6b130!2=8fpl7t)0%kmilm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
