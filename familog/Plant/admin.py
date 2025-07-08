0c2b2aa76acfc6d58bcdc53d1e515f8a6dd5da05
Jinacker <rlawls1448@naver.com>
2025-07-08 11:42:23 +0900

    [MODEL] Plant 총 물준 횟수 추가

--- familog/Plant/admin.py
+++ familog/Plant/admin.py
@@ -5,7 +5,7 @@ from .models import Plant
 
 @admin.register(Plant)
 class PlantAdmin(admin.ModelAdmin):
-    list_display  = ("id", "family", "type", "grow_level", "last_watered")
+    list_display  = ("id", "family", "type", "grow_level", "watering_count")
     list_filter   = ("type",)
 
     search_fields = ("family__code", "type")   # family 코드·식물 타입으로 자동완성 검색
--- familog/Plant/models.py
+++ familog/Plant/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Plant(models.Model):
-    family       = models.OneToOneField("Family.Family",
-                                        on_delete=models.CASCADE,
-                                        related_name="plant")
-    type         = models.CharField(max_length=30)
-    grow_level   = models.PositiveIntegerField(default=0)
-    last_watered = models.DateTimeField(null=True, blank=True)
+    family         = models.OneToOneField("Family.Family",
+                                          on_delete=models.CASCADE,
+                                          related_name="plant")
+    type           = models.CharField(max_length=30)
+    grow_level     = models.PositiveIntegerField(default=0)
+    watering_count = models.PositiveIntegerField(default=0)  # 물 준 횟수
 
     def __str__(self):
         return f"Plant-{self.family.code}"
