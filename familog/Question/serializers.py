0848ae53a6f493ac771c40bfa54efedcbec196de
Jinacker <rlawls1448@naver.com>
2025-07-10 19:01:35 +0900

    [FIX] 시리얼라이저 수정

--- familog/Question/serializers.py
+++ familog/Question/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 from .models import Question, QuestionPool
 
+from rest_framework import serializers
+from .models import Question, QuestionPool
+
 class QuestionSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     date = serializers.DateField(source='q_date', read_only=True)
     content = serializers.CharField(read_only=True)
     is_completed = serializers.BooleanField(read_only=True)
+    code = serializers.CharField(source='family.code', read_only=True)  # ✅ 이거 추가
 
     class Meta:
         model = Question
-        fields = ("id", "date", "content", "is_completed")
+        fields = ("id", "date", "content", "is_completed", "code")  # ✅ 포함시켜야 함
+
 
 class QuestionPoolCreateSerializer(serializers.ModelSerializer):
     class Meta:
